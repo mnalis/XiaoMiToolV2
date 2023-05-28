1e5ea186fc204917bc9a469bd6dea66c0f34b2ee
jk2K <4025839+jk2K@users.noreply.github.com>
2023-05-28 23:44:55 +0800

    fix: support mac m1

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 
     // Apply the application plugin to add support for building a CLI application.
     application
-    id("org.openjfx.javafxplugin") version "0.0.9"
+    id("org.openjfx.javafxplugin") version "0.0.13"
 
 }
 
@@ -32,7 +32,7 @@ java {
 }
 
 javafx {
-    version = "19.0.2.1"
+    version = "20.0.1"
     modules = listOf("javafx.controls", "javafx.fxml", "javafx.web", "javafx.swing")
 }
 
